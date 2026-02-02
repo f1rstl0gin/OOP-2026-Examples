42559e83820a2f037db844b62e78a50328a56fe2
Jeremy Callinan <jeremy.callinan@gmail.com>
2026-02-01 23:29:43 -0500

    Fixing the MAUI mobile app

---
+++ week4/CallinanBank/CallinanBankMaui/Platforms/Windows/App.xaml.cs
@@ -0,0 +1,10 @@
+namespace CallinanBankMaui.WinUI;
+
+public partial class App : MauiWinUIApplication
+{
+    public App()
+    {
+    }
+
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
